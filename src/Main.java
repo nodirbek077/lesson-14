89686c361ee0b9906bb274c0386279930f2c97aa
Nodirbek <nodirbeknurqulov096@gmail.com>
2026-01-15 09:24:03 +0500

    several constructors

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
         person1.name = "Alish";
         person1.surname = "Aliyev";
 
-        Person person2 = new Person();
+        Person person2 = new Person("Valish");
         person2.name = "Valish";
+
+        Person person3 = new Person("Toshmat", "Toshmatov");
 
         System.out.println(person1.name + " " + person1.surname);
         System.out.println(person2.name);
--- src/Person.java
+++ src/Person.java
@@ -3,6 +3,14 @@ public class Person {
     String surname;
 
     public Person(){
-        System.out.println("Person constructor");
+        System.out.println("Person Constructor");
+    }
+
+    public Person(String personName) {
+        System.out.println(personName);
+    }
+
+    public Person(String personName, String personSurname) {
+        System.out.println(personName + " " + personSurname);
     }
 }
